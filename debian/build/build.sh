99f8ee1e667e8d5a1d14195de7fbde414a537028
Jon Henrik Bjørnstad <jonhenrik@qbee.io>
2025-09-16 20:56:43 +0200

    do not use kvm

--- debian/build/build.sh
+++ debian/build/build.sh
@@ -41,7 +41,7 @@ if [[ "$BUILD_ARCH" == "arm64" ]]; then
     $QEMU_OPTIONS
 elif [[ "$BUILD_ARCH" == "amd64" ]] && [[ -c /dev/kvm ]]; then
   #QEMU_OPTIONS="$QEMU_OPTIONS -machine type=pc,accel=kvm -smp 4 -cpu host -nographic"
-  QEMU_OPTIONS="$QEMU_OPTIONS -machine type=pc -smp 4 -cpu host -nographic"
+  QEMU_OPTIONS="$QEMU_OPTIONS -machine type=pc -smp 4 -cpu max -nographic"
   qemu-system-${QEMU_ARCH} \
   -m 1G \
     -device virtio-net-pci,netdev=net0,mac=$MAC \
